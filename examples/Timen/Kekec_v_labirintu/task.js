78254c037e731bce1b580a979aba38d209d19045
timenbob <timen.bobnar@gmail.com>
2024-06-11 13:53:48 +0200

    kekec

--- examples/Timen/Kekec_v_labirintu/task.js
+++ examples/Timen/Kekec_v_labirintu/task.js
@@ -9,7 +9,7 @@ function initTask(subTask) {
 			}
 		},
 		// local library
-		// CUSTOM: here you can override the translation from blocklyRobot_lib.js
+		// CUSTOM: here you can override the tr anslation from blocklyRobot_lib.js
 		// the dictionary tree can be incomplete (have missing values),
 		// but the architecture needs to be the same as: "localLanguageStrings"
 		languageStrings: {
@@ -49,12 +49,13 @@ function initTask(subTask) {
 		},
 		actionDelay: 400,				//parameter za časovni zamik med izvajanjem ukazov -  ne deulje??
 		blocklyColourTheme: "bwinf",	//izbira seta barv za bloke ukazov
-		maxInstructions: 100,
+		//backgroundColour: "purple"
+		maxInstructions: 100,    //omejitve stevila ukazov(0==neskončno delčkov)==================================================
 		includeBlocks: {						//dovoljeni ukazi 
 			groupByCategory: true,
-			generatedBlocks: {
+			generatedBlocks: {//delčki iz blocklyRobot_lib.js
 				robot:  [
-					"move",
+					"move",  //kako se lahko robot premika
 					"turn",
 					"turnAround",
 					"changeRobot",
@@ -62,11 +63,11 @@ function initTask(subTask) {
 				],
 				// robot:  ["left","right","north","west","east","south","changeRobot", "pickTransportable","dropTransportable"],
 			},
-			standardBlocks: {
+			standardBlocks: {  //če dodajam iz francoskega že narejene delčke(obsataj file teh definicij)===============================
 				includeAll: false,
-				wholeCategories: [],
-				singleBlocks: ["controls_repeat_ext"],
-				excludedBlocks: [],
+				wholeCategories: [],//dodamo celotno kategorijo(lopps, functions,text)================
+				singleBlocks: ["controls_repeat_ext"],//posamezne delčke kot controls_if, list_getIndex, list_sort...
+				excludedBlocks: [],//iz dosedanjega nabora delčkov lahko izključimo posamezne delčke====================
 			},
 		},
 		startingExample: { //vnaprej podana koda ukazov
@@ -74,31 +75,39 @@ function initTask(subTask) {
 			// blockly: '<xml xmlns="http://www.w3.org/1999/xhtml"><block type="robot_start" id="Yx#}`-PvOO]HA4c0m7]F" deletable="false" movable="false" editable="false" x="0" y="0"><next><block type="move" id="4,KCbSD;_m-pZxtufj95"><field name="PARAM_0">S</field><value name="PARAM_1"><shadow type="math_number" id="CIspNjAYb7a*vulaV1{P"><field name="NUM">2</field></shadow></value><next><block type="controls_repeat_ext" id="xz}GE@0Jjd*qSArjsPK7"><value name="TIMES"><shadow type="math_number" id="tt~vC4c`SoXp*D0{g`g4"><field name="NUM">10</field></shadow></value><statement name="DO"><block type="move" id="WeEDtrj7~6yjk(UWz0-I"><field name="PARAM_0">NW</field><value name="PARAM_1"><shadow type="math_number" id="opp6D`R,=GUHmyGwa.R?"><field name="NUM">2</field></shadow></value><next><block type="move" id="@1V??r6T.~WNU!1eUDFK"><field name="PARAM_0">W</field><value name="PARAM_1"><shadow type="math_number" id="0{K)gjXQYd/@ACLa1Zth"><field name="NUM">2</field></shadow></value><next><block type="move" id="z)fMk;vuUbh5Da[KR*xj"><field name="PARAM_0">SW</field><value name="PARAM_1"><shadow type="math_number" id="wB9]`B@g|8|}GB/1Tm@!"><field name="NUM">2</field></shadow></value><next><block type="move" id="GeZy`Ver;W!z`XSw45r`"><field name="PARAM_0">S</field><value name="PARAM_1"><shadow type="math_number" id="sZB.rV,77Bqvtrj0kOBO"><field name="NUM">2</field></shadow></value><next><block type="move" id="M17`JoUY;BMn/l@i}fsy"><field name="PARAM_0">SE</field><value name="PARAM_1"><shadow type="math_number" id="Hd_3qqIk*y;,]lM_qE@,"><field name="NUM">2</field></shadow></value><next><block type="move" id="k}eq{We-_F/f])5ibq*S"><field name="PARAM_0">E</field><value name="PARAM_1"><shadow type="math_number" id="gu?bUo7P.xnCNvY|6aH#"><field name="NUM">2</field></shadow></value><next><block type="move" id="R/G.u-]Ep6Moi)~0{B}7"><field name="PARAM_0">NE</field><value name="PARAM_1"><shadow type="math_number" id="c1;Ku-whH9kw@[,TwuQK"><field name="NUM">2</field></shadow></value><next><block type="move" id="O9,[~_q4s8-*m-C/wuRr"><field name="PARAM_0">N</field><value name="PARAM_1"><shadow type="math_number" id="QI=mOSVV5;~2s5DK@fCD"><field name="NUM">2</field></shadow></value></block></next></block></next></block></next></block></next></block></next></block></next></block></next></block></statement></block></next></block></next></block><additional>{}</additional></xml>',
 			// blockly: '<xml xmlns="http://www.w3.org/1999/xhtml"><block type="robot_start" id="Yx#}`-PvOO]HA4c0m7]F" deletable="false" movable="false" editable="false" x="0" y="0"><next><block type="move" id="4,KCbSD;_m-pZxtufj95"><field name="PARAM_0">W</field><value name="PARAM_1"><shadow type="math_number" id="CIspNjAYb7a*vulaV1{P"><field name="NUM">3</field></shadow></value><next><block type="destroy" id="KKQSCxKu;sV}+gJe?9=)"><field name="PARAM_1">obstacle</field><field name="PARAM_0">2</field><next><block type="move" id="XWv:_2Qbckm|{-94@`u,"><field name="PARAM_0">N</field><value name="PARAM_1"><shadow type="math_number" id="oR6vzE-h~{7oseb?hIAf"><field name="NUM">1</field></shadow></value></block></next></block></next></block></next></block><additional>{}</additional></xml>', 
 		},					
-		checkEndEveryTurn: false,		//kako pogosto preverjamo uspešnost rešitve
-		checkEndCondition:  (context, lastTurn) => { 
-			robotEndConditions.checkItemExistence(context, lastTurn, {type: "coin"}, {}, exist=false)
-			
+		checkEndEveryTurn: false,		                              //kako pogosto preverjamo uspešnost rešitve
+		checkEndCondition:  (context, lastTurn) => {                  // funkcija za preverjanje vspešnosti
+			robotEndConditions.checkItemExistence(context, lastTurn, {type: "coin"}, {}, exist=false) // vse možnosti najdemo v blocklyRobot_lib.js(2069) var robotEndConditions
+			//za kombinirane uporabljamo funvkije primer: na datoteki kombinacija \\ Več pogojev
 		},
 		computeGrade: robotGradeFunctions.allOrNothing,
 			
 		border: 0.01,
 		backgroundColour: false,
-		backgroundTile: 'Trava.png',
-		borderColour: 'green',
+		backgroundTile: '',
+		borderColour: 'Wight',
 
-		cellSide: 60,	
+		cellSide: 60,  	
 		numberOfRobots: 2,
 		// only categories: robot, obstacle, transportable, coin, button --> are HARDCODED
-		itemTypes: {
-			robot0: { img: ["girl_all_8_sides.png",   "girl_with_ball_all_8_sides.png"], side: 70, nbStates: 8, offsetX: -6, zOrder: 8, category: {'robot': true}, },
-			robot1: { img: ["dog_all_8_sides.png"], side: 70, nbStates: 8, offsetX: -6, zOrder: 8, category: {'robot': true, 'transportable': true}, transOrder: 0 },
-			obstacle: { num: 2, img: "Trava.png", zOrder: 2, category: {'obstacle': true}},
-			button: { img: ["button_unpressed.png", "button_pressed.png"], zOrder: 2, category: {'button':true} },
-			door: { img: ["orange_floor.png", "grass4.png"], zOrder: 2, category: [{'obstacle':true}, {'obstacle':false}]},
-			goal: { num: 3, side: 80, img:"flag2.png" },
-			marble: {num: 4, img: ["ball.png"], zOrder: 1, category: {'transportable':true},transOrder: 0},
-			hole: {num: 5, img: ["goal.png"], zOrder: 1, },
-			coin: {num:6, img:["yellow_house.png"],zOrder: 2, category:{"coin":true}}
+
+		// offsetX/y lahko grafiko mal prestavlamo (da je čes več polij)
+		// zOrder če imamo več objektov na isti celici to definira klakšen bo vrstni red prikaza
+		// isTransportable ali junak lahko pobere naš objekt
+		// za size velikokrat uporabikar cellSize
+		//
+		itemTypes: {                                                                                            
+			robot0: { img: ["Kekec.png",   "Kekec.png"], side: 70, nbStates: 1, offsetX: -6, zOrder: 8, category: {'robot': true}, },
+			obstacle: { num: 2, img: "Trava.png", zOrder: 3, category: {'obstacle': true}},
+			obstacle2: { num: 3, img: "Mojca.png",offsetY: -20, zOrder: 4, category: {'obstacle': true}},
+			roza1:{num: 4, img:"Roza1.png"},
+			roza2:{},
+			//button: { img: ["button_unpressed.png", "button_pressed.png"], zOrder: 2, category: {'button':true} },
+			//door: { img: ["orange_floor.png", "grass4.png"], zOrder: 2, category: [{'obstacle':true}, {'obstacle':false}]},
+			//goal: { num: 3, side: 80, img:"flag2.png" },
+			//marble: {num: 4, img: ["ball.png"], zOrder: 1, category: {'transportable':true},transOrder: 0},
+			//hole: {num: 5, img: ["goal.png"], zOrder: 1, },
+			coin: {num:6, img:["hisa.png"],zOrder: 1, category:{"coin":true}}
 		}, 
 		ignoreInvalidMoves: false,
 	};
@@ -107,23 +116,21 @@ function initTask(subTask) {
 		easy: [
 			{
 				tiles: [
-					[1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-					[1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-					[1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-					[1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-					[1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-					[1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-					[1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-					[1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-					[1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-					[1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+					[2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+					[2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 2],
+					[2, 2, 2, 1, 2, 2, 2, 2, 2, 1, 2],
+					[2, 2, 2, 1, 2, 1, 1, 1, 2, 1, 2],
+					[2, 2, 2, 1, 2, 6, 2, 1, 2, 1, 2],
+					[2, 2, 2, 1, 2, 2, 3, 1, 2, 1, 2],
+					[2, 2, 2, 1, 1, 1, 1, 1, 2, 1, 2],
+					[2, 1, 2, 2, 2, 2, 2, 2, 2, 1, 2],
+					[2, 1, 4, 1, 1, 1, 1, 1, 1, 1, 2],
+					[2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
 					
 				],
-				initItems: [
-					{ row: 0, col: 7, dir: 0, type: "robot0" },
-					{ row: 4, col: 0, dir: 0, type: "robot1" },
-					{row:3,col:3,type:"button",id:1},
-					{row:4,col:2,type:"door",id:1},
+				initItems: [ //direktno dodamo elemente
+					{ row: 7, col: 1, dir: 0, type: "robot0" },
+				
 				
 				],
 			},
